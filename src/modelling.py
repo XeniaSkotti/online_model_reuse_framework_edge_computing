fb1922a8e3fa8ece9bd8cc91a1e29b8f940f3492
Xenia Skotti <46594038+XeniaSkotti@users.noreply.github.com>
2021-10-12 13:41:21 +0100

    replaced print statement with PrettyTable

--- src/modelling.py
+++ src/modelling.py
@@ -1,11 +1,12 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
 from sklearn.model_selection import GridSearchCV
+from prettytable import PrettyTable
 
 def instantiate_clf(name):
     if name == "linear":
         return LinearRegression()
-    elif name == "svr"
+    elif name == "svr":
         return SVR()
 
 def fit_clf(clf, train, test):
@@ -18,23 +19,24 @@ def score_clf(clf, test):
 
 def grid_search_models(clf_name, model_data, selected_nodes, param_grid):
     models = {}
+    t = PrettyTable(['Node', 'Model', 'Coefficient of Determination (R)'])
     for node in selected_nodes:
         train, test = model_data[node] 
         
         model = instantiate_clf(clf_name)
         score = fit_clf(model, train, test)
-        print(f"Baseline Model Coeefficient of Determination for node {node}: {score}")
+        t.add_row([node, model, score])
 
         grid_search = GridSearchCV(instantiate_clf(clf_name), param_grid)
         grid_search.fit(train[:,1].reshape(-1,1), train[:,0])
-        best_params = grid_search.best_params_
 
-        print(f"The best parameter settings for node {node} are: {best_params}")
         model = instantiate_clf(clf_name)
-        model.set_params(**best_params)
+        model.set_params(**grid_search.best_params_)
         score = fit_clf(model, train, test)
-        print(f"Optimised Model Coeefficient of Determination for node {node}: {score} \n")
+        t.add_row([node, model, score])
         
         models[node] = model
-
+    
+    print(t)
+    
     return models

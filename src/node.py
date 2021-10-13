b81a24cd7dd53076cb580140c14f72917aa3c38b
Xenia Skotti <46594038+XeniaSkotti@users.noreply.github.com>
2021-10-13 11:56:49 +0100

    added functionality to determine with OCSVM the similar_pairs

--- src/node.py
+++ src/node.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.svm import OneClassSVM
+from itertools import combinations as comb
 
 def get_similar_other_nodes_sets(experiment):
     if experiment == 1:
@@ -14,13 +15,18 @@ def get_similar_other_nodes_sets(experiment):
         other_nodes = ["pi2", "pi4"]
     return similar_nodes, other_nodes
 
-def get_node_data(data, experiment):
+def get_node_data(data, experiment, filtered = True):
     exp = data.loc[data.experiment==experiment][["humidity", "temperature"]]
     a = exp.loc[data.pi=="pi2"]
     b = exp.loc[data.pi=="pi3"]
     c = exp.loc[data.pi=="pi4"]
     d = exp.loc[data.pi =="pi5"]
-    return remove_outliers([a,b,c,d])
+    
+    node_data = [a,b,c,d]
+    if filtered:
+        return remove_outliers(node_data)
+    else: 
+        return node_data
 
 def remove_outliers(node_data):
     for i in range(4):
@@ -30,3 +36,39 @@ def remove_outliers(node_data):
         node_data[i] = node_data[i].iloc[inliers]
         
     return node_data
+
+def get_similar_pairs_unidirectional(node_data):
+    predicted_inliers = []
+    models = []
+
+    for i in range(4):
+        model = OneClassSVM(nu=0.1)
+        pred = model.fit_predict(node_data[i])
+        inliers = np.where(pred == 1)
+        predicted_inliers.append(inliers[0])
+        models.append(model)
+
+    combos = comb(range(4),2)
+
+    similar_pairs = []
+    for c in combos:
+        x, y = c[0], c[1]
+
+        model_x, model_y = models[x], models[y]
+        inliers_x, inliers_y = predicted_inliers[x], predicted_inliers[y]
+
+        predicted_y_inliers = np.where(model_x.predict(node_data[y]) == 1)
+        predicted_x_inliers = np.where(model_y.predict(node_data[x]) == 1)
+
+        int_x = np.intersect1d(inliers_x, predicted_x_inliers)
+        int_y = np.intersect1d(inliers_y, predicted_y_inliers)
+
+        x_y_overlap = int_x.shape[0]/inliers_x.shape[0]
+        y_x_overlap = int_y.shape[0]/inliers_y.shape[0]
+
+        if y_x_overlap > 0.85:
+            similar_pairs.append(("pi"+str(x+2), "pi"+str(y+2)))
+        if x_y_overlap > 0.85:
+            similar_pairs.append(("pi"+str(y+2), "pi"+str(x+2)))
+    
+    return similar_pairs
